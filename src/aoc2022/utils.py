a65a4de1715d09f766d567cf040a780df1c61f96
Tomer Gabay <tomergabay@Admins-MacBook-Pro.local>
2022-12-08 16:46:45 +0100

    created utils module with timeit function

---
+++ src/aoc2022/utils.py
@@ -0,0 +1,20 @@
+import time
+from functools import wraps
+from statistics import fmean, stdev
+
+
+def timeit(n: int = 10):
+  def decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        exec_times = []
+        for _ in range(n):
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            exec_times.append(time.time() - start_time)
+        mean = fmean(exec_times)
+        rel_stdev = round((stdev(exec_times) / mean) * 100,2)
+        print(f"'{func.__name__}()' took on average {mean} seconds with a stdev of {rel_stdev}%. ({n} runs).")
+        return result
+    return wrapper
+  return decorator
