286e1c55b2cf2f7fa85a257d5fa05ac5be62ef57
Tomer Gabay <tomergabay@Admins-MacBook-Pro.local>
2022-12-11 14:42:42 +0100

    day11

--- src/aoc2022/utils.py
+++ src/aoc2022/utils.py
@@ -1,6 +1,8 @@
+import re
 import time
 from functools import wraps
 from statistics import fmean, stdev
+from typing import Optional
 
 
 def timeit(n: int = 10):
@@ -18,3 +20,8 @@ def timeit(n: int = 10):
         return result
     return wrapper
   return decorator
+
+
+def get_numbers_from_string(string: str, n_numbers: Optional[int] = None, type_: type = int):
+  numbers = list(map(type_, re.findall(r'\b\d+\b', string)))
+  return numbers if n_numbers is None else numbers[:n_numbers]
